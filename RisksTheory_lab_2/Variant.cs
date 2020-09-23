00ba75a39a3bde6dc940253d9f606c4acbde76e7
MKovblyuk <49000927+MKovblyuk@users.noreply.github.com>
2020-09-23 20:22:10 +0300

    Add files via upload

---
+++ RisksTheory_lab_2/Variant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RisksThery_Lab_2
+{
+    class Variant
+    {
+        public double probSuccess;
+        public double probFail;
+        public double incomeSuccess;
+        public double incomeFail;
+
+        public Variant()
+        {
+            probSuccess = 0;
+            probFail = 0;
+            incomeSuccess = 0;
+            incomeFail = 0;
+        }
+        public Variant(double probS,double probF,double incomeS, double incomeF)
+        {
+            probSuccess = probS;
+            probFail = probF;
+            incomeSuccess = incomeS;
+            incomeFail = incomeF;
+        }
+    }
+}
